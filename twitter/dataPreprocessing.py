a2d3c9c26e38501a2d9bd2eea6d9bee163e06614
r05921104 <r05921104@ntu.edu.tw>
2017-07-29 03:42:31 +0000

    modify train and test

--- twitter/dataPreprocessing.py
+++ twitter/dataPreprocessing.py
@@ -5,6 +5,7 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.embeddings import Embedding
 import numpy as np
+# training data
 lx=[]
 ly=[]
 with open('train.csv') as file:
@@ -16,6 +17,11 @@ with open('train.csv') as file:
 			continue
 		lx.append(line[2])
 		ly.append(int(line[1]))
+		#print str(count)+' '+str(line[0])
+                #if count != int(line[0]):
+                #       print str(line[0])+str(count)
+                #       break
+		count = count +1
 X_train = np.array(lx)
 y_train = np.array(ly)
 
@@ -26,7 +32,29 @@ X_train = token.texts_to_sequences(X_train)
 X_train = sequence.pad_sequences(X_train, maxlen=100)
 
 print token.document_count
+print y_train.shape
+#test
+ltestx=[]
+with open('test.csv') as file:
+        lines = csv.reader(file)
+        count = 0
+        for line in lines:
+                if count == 0:
+                        count = count+1
+                        continue
+                ltestx.append(line[1])
+		#print str(count)+' '+str(line[0])
+		#if count != int(line[0]):
+		#	print str(line[0])+str(line[1])+str(count)
+		#	break
+		count = count + 1
 		
+X_test = np.array(ltestx)
+X_test = token.texts_to_sequences(X_test)
+X_test = sequence.pad_sequences(X_test, maxlen=100)
+print X_test.shape
+
+# keras training		
 model = Sequential()
 model.add(Embedding(output_dim=32,input_dim=2000,input_length=100))
 model.add(Dropout(0.2))
@@ -38,20 +66,7 @@ model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
 model.fit(X_train,y_train,batch_size=100,epochs=15)
 
 
-#test
-ltestx=[]
-with open('test.csv') as file:
-	lines = csv.reader(file)
-	count = 0
-	for line in lines:
-		if count == 0:
-			count = count+1
-			continue
-		ltestx.append(line[1])
-X_test = np.array(ltestx)
-
-X_test = token.texts_to_sequences(X_test)
-X_test = sequence.pad_sequences(X_test, maxlen=100)
+# keras predict
 		
 predict = model.predict(X_test)
 print predict
